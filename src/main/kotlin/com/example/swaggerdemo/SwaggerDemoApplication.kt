2612d93ce02ab3787922307554f63d96a5b0e3e5
msosa <m.sosa1127@gmail.com>
2023-03-01 16:49:39 +0800

    initial commit

---
+++ src/main/kotlin/com/example/swaggerdemo/SwaggerDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swaggerdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwaggerDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwaggerDemoApplication>(*args)
+}
